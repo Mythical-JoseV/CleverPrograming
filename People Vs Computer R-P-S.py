501d37fcb7aead6c1e3afdcae78968ef41bc91bc
Mythical-JoseV <46796550+Mythical-JoseV@users.noreply.github.com>
2019-02-07 13:45:00 -0700

    Made more user friendly. Changed inputs.

--- People Vs Computer R-P-S.py
+++ People Vs Computer R-P-S.py
@@ -1,15 +1,30 @@
+#####R-P-S
+##Jose Vest
+#####2.7.2019
+
 #Loop Start
 while True:
 
-# This is for random number values of each rock, paper, or scissor
+#This is for random number values of each rock, paper, or scissor
     import random
 
 #This is the input of the player and the outcomes
-    Player1 = int(input("Hello Player call your outcome; 1, 2, 3."))
+    Player1 = input("Hello Player call your outcome; Rock, Paper, Scissors.")
+    
     Player2 = random.randint(1, 3)
 
+    if Player1.lower() == "rock":
+      Player1 = 1
+
+    elif Player1.lower() == "paper":
+      Player1 = 2
+
+    elif Player1.lower() == "scissors":
+      Player1 = 3
+
+  
 #Outcome Start
-    if Player1 == 1 and Player2 == 1:
+    if Player1 == 1  and Player2 == 1 :
       print ("Draw try agian!")
       
 
@@ -42,8 +57,8 @@ while True:
     
     Cont = input("Do you want to try again?")
          
-    if Cont.strip() == 'No':
+    if Cont.lower() == 'no':
       break
     
-    if Cont.strip() == 'Yes':
+    if Cont.lower() == 'yes':
       print("Great!")
